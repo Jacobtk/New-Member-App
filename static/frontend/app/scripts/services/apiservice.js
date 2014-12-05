9f7272052fd48eb18d241ec5d50e2620478438b7
Chris Belyeu <cbelyeu@verisage.us>
2014-12-05 12:43:28 -0700

    added api service for making http requests to the api

--- static/frontend/app/scripts/services/apiservice.js
+++ static/frontend/app/scripts/services/apiservice.js
@@ -1,11 +1,61 @@
-var app = angular.module('workloadManagementApp');
+var app = angular.module('frontendApp');
 
-    app.factory('Project', ['$resource', 'apiRoot',
-        function($resource, apiRoot){
-            var result = $resource(apiRoot + 'projects/:id', {id: '@id'},
-            {
-              'update': { method:'PUT' }
-            });
-            return result;
-        }
-    ]);
+app.factory('Member', ['$resource', 'apiRoot',
+    function($resource, apiRoot){
+        var result = $resource(apiRoot + 'members/:id', {id: '@id'},
+        {
+          'update': { method:'PUT' }
+        });
+        return result;
+    }
+]);
+
+app.factory('Household', ['$resource', 'apiRoot',
+    function($resource, apiRoot){
+        var result = $resource(apiRoot + 'households/:id', {id: '@id'},
+        {
+          'update': { method:'PUT' }
+        });
+        return result;
+    }
+]);
+
+app.factory('Survey', ['$resource', 'apiRoot',
+    function($resource, apiRoot){
+        var result = $resource(apiRoot + 'surveys/:id', {id: '@id'},
+        {
+          'update': { method:'PUT' }
+        });
+        return result;
+    }
+]);
+
+app.factory('ChurchUnit', ['$resource', 'apiRoot',
+    function($resource, apiRoot){
+        var result = $resource(apiRoot + 'units/:id', {id: '@id'},
+        {
+          'update': { method:'PUT' }
+        });
+        return result;
+    }
+]);
+
+app.factory('Member', ['$resource', 'apiRoot',
+    function($resource, apiRoot){
+        var result = $resource(apiRoot + 'members/:id', {id: '@id'},
+        {
+          'update': { method:'PUT' }
+        });
+        return result;
+    }
+]);
+
+app.factory('Address', ['$resource', 'apiRoot',
+    function($resource, apiRoot){
+        var result = $resource(apiRoot + 'addresses/:id', {id: '@id'},
+        {
+          'update': { method:'PUT' }
+        });
+        return result;
+    }
+]);

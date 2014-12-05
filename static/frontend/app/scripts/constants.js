03504143b7162ee704620e316abc011906396794
Chris Belyeu <cbelyeu@verisage.us>
2014-12-05 12:42:51 -0700

    added constants file

---
+++ static/frontend/app/scripts/constants.js
@@ -0,0 +1,21 @@
+var constants = angular.module('frontendApp.constants', []);
+
+this.createServerRoot = function(){
+  var currentUrl = window.location.href;
+
+  if(currentUrl.indexOf('localhost') > -1 || currentUrl.indexOf('127.0.0') > -1){
+    return 'http://localhost:8000/api/';
+  } else{
+    return 'http://newmemberapp.herokuapp.com/api/';
+  }
+};
+
+this.createApiVersion = function(){ return 'v1'};
+
+this.createApiRoot = function(){
+  return this.createServerRoot() + this.createApiVersion() + '/';
+};
+
+constants.constant('serverRoot', this.createServerRoot());
+constants.constant('apiVersion', this.createApiVersion());
+constants.constant('apiRoot', this.createApiRoot());

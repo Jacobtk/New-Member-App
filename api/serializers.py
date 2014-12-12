1f232f9473b18d590808ceca1e9668a9142b1780
Chris Belyeu <cbelyeu@verisage.us>
2014-12-12 00:39:32 -0700

    expanded serializers

--- api/serializers.py
+++ api/serializers.py
@@ -4,6 +4,8 @@ from models import Household
 from models import Survey
 from models import ChurchUnit
 from models import Address
+from models import CustomField
+from models import CustomFieldEntry
 from django.contrib.auth.models import User
 
 
@@ -23,8 +25,14 @@ class SurveySerializer(serializers.ModelSerializer):
 
 
 class ChurchUnitSerializer(serializers.ModelSerializer):
+    bishop = MemberSerializer()
+    custom_fields = serializers.SerializerMethodField('get_custom_fields')
+
     class Meta:
         model = ChurchUnit
+        fields = (
+            'id', 'bishop', 'name'
+        )
 
 
 class AddressSerializer(serializers.ModelSerializer):
@@ -35,3 +43,13 @@ class AddressSerializer(serializers.ModelSerializer):
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
+
+
+class CustomFieldSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomField
+
+
+class CustomFieldEntrySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomFieldEntry

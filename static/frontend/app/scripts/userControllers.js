fbfe6e603a8590344cf83993c43b25c11360c220
Chris Belyeu <cbelyeu@verisage.us>
2014-12-12 00:40:29 -0700

    added resource call for custom fields by unit to userControllers

--- static/frontend/app/scripts/userControllers.js
+++ static/frontend/app/scripts/userControllers.js
@@ -1,10 +1,18 @@
 'use strict';
 
-var newMemberAppControllers = angular.module('newMemberAppControllers', []);
+var newMemberAppControllers = angular.module('newMemberApp');
 
-newMemberAppControllers.controller('UserController', ['$scope', '$routeParams',
-function($scope, $routeParams) {
-	
+newMemberAppControllers.controller('UserController', ['$scope', '$routeParams', '$resource', 'apiRoot',
+function($scope, $routeParams, $resource, apiRoot) {
+
+  $scope.unit = $resource(apiRoot + 'unit').query();
+  $scope.customFields = $resource(apiRoot + 'unit/' + $routeParams.unitID + '/entries');
+
+  $scope.foo = apiRoot;
+  $scope.members = $resource(apiRoot + 'members').query();
+
+
+
 	$scope.unitName = $routeParams.unitName;
 	$scope.fields = [1,2,3,45,6,7844
 
@@ -13,7 +21,6 @@ function($scope, $routeParams) {
   	$scope.currentUser = "Bob";
   	$scope.submitted = false;
 /*  get fields from the database   */
-  
 }]);
 
 newMemberAppControllers.controller('SigninController', ['$scope',
